5cb00084c58854bac4d6011fc4d9b1cb0e726486
rahul_dudhane <rahul.y.dudhane@gmail.com>
2020-02-29 11:17:18 +0530

    initial commit

---
+++ src/app/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { HomeComponent } from "./home/home.component";
+
+
+@NgModule({
+    declarations:[
+        HomeComponent
+    ],
+    exports:[
+        HomeComponent
+    ]
+})
+export class HomeModule{
+
+}
